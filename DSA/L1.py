34acc4294907d7fbfefb95ec6a9b35c5d44c6f2a
Jyoti Sharma <jyotisharma4213@gmail.com>
2026-02-12 15:19:20 +0530

    first commit

---
+++ DSA/L1.py
@@ -0,0 +1,14 @@
+rows=3
+cols=4
+firstMatrix=[]
+
+for i in range(rows):
+    rowArray=[]
+    for j in range(cols):
+        rowArray.append(int(input("Enter the value of the matrix: ")))
+    firstMatrix.append(rowArray)
+
+print(firstMatrix)
+
+
+#traversal of matrix
