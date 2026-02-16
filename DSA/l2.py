7c96d004f752d34cb87d3ce078d1c4361eefce9f
Jyoti Sharma <jyotisharma4213@gmail.com>
2026-02-16 13:15:35 +0530

    more detail

--- DSA/l2.py
+++ DSA/l2.py
@@ -1,4 +1,19 @@
 #Diagonal traversal
 #primary diagonal: (row index=column index)
+
+from matplotlib.pylab import matrix
+from pyparsing import col
+
+
+rows=3
+for i in range(rows):
+    print(matrix[i][i])
+
 #Secoundary diagonal: ro index+column=total no rows-1
+
+c=cols-1
+for i in range(rows):
+    print(matrix[i][c])
+    c-=1
+
 
